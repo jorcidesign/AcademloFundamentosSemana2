c40e6b32bc69fbba5207470ceae5afd5086d738e
jorcidesign <jorci.design@gmail.com>
2023-12-01 18:54:41 -0500

    bucles

--- 07-operadores-de-comparacion/operadores-de-comparacion.js
+++ 07-operadores-de-comparacion/operadores-de-comparacion.js
@@ -3,3 +3,91 @@
   * Operadores de comparación *
 =================================
 */
+// Los operadores de comparación en JavaScript son utilizados para comparar dos valores y devolver un resultado booleano que indica si la comparación es verdadera o falsa.
+
+// => Relacionales
+// > Mayor que
+console.log(5 > 3) // true
+console.log(5 > 10) // false
+// < Menor que
+console.log(3 < 5) // true
+console.log(10 < 5) // false
+// >= Mayor o igual que
+console.log(5 >= 5) // true > =
+// <= Menor o igual que
+console.log(0 <= 0)
+
+// => Igualdad (comparación simple)
+// - comparan los valores de dos expresiones
+// == Igual que ==
+// != Diferente de !=
+console.log(10 == 10)
+console.log(10 != 10)
+
+// => Comparación de cadenas de texto:
+// - Comparan el orden lexicográfico de los caracteres Unicode de las cadenas.
+// - El orden lexicográfico es el orden alfabético de las cadenas.
+console.log('A' > 'a')
+console.log(65 > 97)
+console.log('a' > 'z')
+
+// => str.charCodeAt(índice/posición) -> Devuelve el valor Unicode del carácter
+console.log('A'.charCodeAt())
+console.log('a'.charCodeAt())
+
+// Comparación mas compleja:
+'tierra' > 'tierna' // Si coinciden, se compara el siguiente carácter
+console.log('r'.charCodeAt())
+console.log('n'.charCodeAt())
+console.log('tierra' > 'tierna')
+console.log('zzz' > 'ab') // la cadena mas corta es menor
+
+// string > number
+// El de igualdad convierte el tipo de dato a number implicitamente
+console.log('15' == 15)
+console.log('abc' == 1)
+
+// Los relacionales comparan los valores individuales de cada cadena
+console.log('15' > 10)
+console.log('5' < '6')
+
+// Boolean true = 1, false = 0 -> number
+console.log(true > false)
+console.log(0 == false)
+console.log(1 == true)
+
+// Regla de conversión númerica
+// null -> 0 (para los relacionales null es igual a 0)
+console.log(null > 0)
+console.log(null < 1)
+console.log(null >= 0)
+
+// undefined -> null
+// null y undefined son iguales entre ellos mismos, pero no a ningún otro valor
+console.log(null == 0)
+console.log(null == undefined)
+console.log(null == null)
+console.log(undefined == undefined)
+console.log(undefined == 0)
+console.log(null == '')
+
+// NaN -> NaN
+// NaN es el unico valor que no es igual a NADA
+console.log(NaN == NaN)
+console.log(NaN == '')
+console.log(NaN == 0)
+console.log(NaN == false)
+console.log(NaN == true)
+
+// Igualdad estricta (===)
+// - Primero compara los tipos, si son diferentes, devuelve false. Si son iguales, se compara el valor
+console.log('5' == 5) // comparción simple
+console.log(true == 1)
+console.log('5' === 5) // comparación estricta
+console.log(true === 1)
+console.log(5 === 5)
+console.log('0' !== 0)
+
+// => Precedencia de operadores:
+// - Los operadores de comparación tienen una precedencia menor que los aritméticos.
+console.log(5 + 5 == 10)
--- 09-numeros/lesson.js
+++ 09-numeros/lesson.js
@@ -3,3 +3,85 @@
   * Números *
 ===============
 */
+// Un tipo de dato primitivo que se utiliza para representar valores numéricos.
+const entero = 100
+const decimal = 85.6
+
+// => Formas de representar un número:
+console.log(1_000_000_000)
+console.log(1e9) // Notación exponencial
+console.log(1e-6) // Notación exponencial
+console.log(0xff) // Hexadecimal
+console.log(0b11111111) // Binario
+console.log(0o377) // Octal
+
+// Funciones, propiedades y métodos para trabajar con números:
+console.log(+'4')
+console.log(parseInt('4px'))
+console.log(parseFloat('4.5px'))
+console.log(parseFloat('4px'))
+
+// Números especiales:
+console.log(1 / 0) // Infinity
+// isFinite(n) Devuelve true si el valor es un número finito.
+console.log(isFinite(15))
+console.log(isFinite('15'))
+console.log(isFinite(Infinity))
+
+// isNaN(n) Devuelve true si el valor no es un número.
+console.log(isNaN(1))
+console.log(isNaN('abc'))
+
+// Métodos del objeto Number:
+console.log(Number.isFinite(15))
+console.log(Number.isFinite('15'))
+console.log(Number.isFinite(Infinity))
+
+console.log(Number.isNaN(1))
+console.log(Number.isNaN('abc'))
+
+// Propiedades del objeto Number:
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+
+// => Métodos para números
+console.log(1 + 2)
+console.log(15.1 + 8.2)
+console.log(0.1 + 0.2)
+// num.toFixed(n): Devuelve un string con un número de decimales indicado.
+console.log(25.6865485.toFixed(5)) // Redondea tal cual como lo hace el método Math.round()
+console.log(5..toFixed(1))
+console.log((0.1 + 0.2).toFixed(1))
+
+console.log(12345678912345678912) // number
+console.log(12345678912345678912n) // BigInt
+
+// num.toString(base): Devuelve un string con el número indicado.
+console.log(100 + '')
+console.log(100..toString(10)) // 10: decimal
+console.log(100..toString(2)) // 2: binario
+console.log(100..toString(8)) // 8: octal
+console.log(100..toString(16)) // 16: hexadecimal
+
+// => Objeto Math: Proporciona propiedades y métodos para realizar tareas matemáticas.
+console.log(Math)
+
+// Propiedades del objeto Math:
+console.log(Math.PI)
+
+// Métodos del objeto Math:
+console.log(Math.round(5.4)) // Redondea al entero más cercano.
+console.log(Math.round(5.5)) // Redondea al entero más cercano.
+console.log(Math.floor(5.9)) // Redondea al entero por debajo.
+console.log(Math.ceil(5.1)) // Redondea al entero por encima.
+
+// Raíz cuadrada
+console.log(Math.sqrt(81)) // devuelve la raíz cuadrada de un número.
+
+// Potencia
+console.log(Math.pow(2, 5)) // devuelve la potencia de un número.
+console.log(2 ** 5) // devuelve la potencia de un número.
+
+// Número aleatorio
+console.log(Math.random()) // devuelve un número aleatorio entre 0 y 1.
+console.log(Math.floor((Math.random() * 10) + 1)) // devuelve un número aleatorio entre 1 y 10.
