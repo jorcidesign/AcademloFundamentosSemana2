21e943475451d3cec2ac0ac758efe0e340bffff8
jorcidesign <jorci.design@gmail.com>
2023-12-01 19:06:39 -0500

    numeros

--- 07-operadores-de-comparacion/test/task.js
+++ 07-operadores-de-comparacion/test/task.js
@@ -1,26 +1,19 @@
 // </ #8 Operadores de comparación />
-
 /*
   Ejercicio #1
   Realiza los siguientes ejercicios de operadores de comparación
   Reeplaza null por true o false según corresponda.
 */
-
 // ¿Cúal es el resultado? => 18 == '18'
-const ejemplo1 = null
-
+const ejemplo1 = true;
 // ¿Cúal es el resultado? => true < false
-const ejemplo2 = null
-
+const ejemplo2 = false;
 // ¿Cúal es el resultado? => null <= 0
-const ejemplo3 = null
-
+const ejemplo3 = true;
 // ¿Cúal es el resultado? => 'tierna' >= 'tierra'
-const ejemplo4 = null
-
+const ejemplo4 = false;
 // ¿Cúal es el resultado? => 0 != ''
-const ejemplo5 = null
-
+const ejemplo5 = false;
 /*
   Ejercicio #2
   Realiza las siguientes funciones de operadores de comparación
@@ -29,44 +22,43 @@ const ejemplo5 = null
   No debes crear otras variables (a menos que los requieras). Utiliza los parámetros que reciben las funciones.
   Recuerda utilizar return para devolver valores.
 */
-
 function sonIguales (num1, num2) {
   // Retorna true si num1 y num2 son iguales, de lo contrario, retorna false.
   // Ten en cuenta que 1 y '1' son tipos diferentes por lo cual deben retornar false.
   // Ejemplo: in: (num1: 18, num2: 18) ~ out: => true
   // Tu código:
+  return (num1 == num2)
 }
-
 function tienenMismaLongitud (str1, str2) {
   // Retorna true si las dos strings tienen el mismo largo, de lo contrario, retorna false.
   // Ejemplo: in: (str1: 'hi', str2: 'ha') ~ out: => true
   // Tu código:
+  return (str1.length == str2.length)
 }
-
 function menosQueNoventa (num) {
   // Retorna true si num es menor que noventa, de lo contrario, retorna false.
   // Ejemplo: in: (num: 89) ~ out: => true
   // Tu código:
+  return (num < 90)
 }
-
 function mayorQueCincuenta (num) {
   // Retorna true si num es mayor que cincuenta, de lo contrario, retorna false.
   // Ejemplo: in: (num: 49) ~ out: => false
   // Tu código:
+  return (num > 50)
 }
-
 function esPar (num) {
   // Retorna true si num es par, de lo contrario, retorna false.
   // Ejemplo: in: (num: 8) ~ out: => true
   // Tu código:
+  return (num % 2 === 0)
 }
-
 function esImpar (num) {
   // Retorna true si num es impar, de lo contrario, retorna false.
   // Ejemplo: in: (num: 7) ~ out: => true
   // Tu código:
+  return (num % 2 !== 0)
 }
-
 // NO TOCAR ESTE CÓDIGO
 module.exports = {
   ejemplo1,
--- 09-numeros/test/task.js
+++ 09-numeros/test/task.js
@@ -1,46 +1,34 @@
 // </ #7 Números />
 
-/*
-  Ejercicio #1
-  Realiza los siguientes ejercicios con números.
-  Reemplaza null por el código necesario para completar el ejercicio.
-*/
+// Ejercicio #1
+//   Realiza los siguientes ejercicios con números.
+//   Reemplaza null por el código necesario para completar el ejercicio.
+// */
 // Utiliza la notación exponencial para representar los siguientes números:
 // Notación exponencial de: 100000 => ??
-const example1 = null
-
+const example1 = 1e5
 // Notación exponencial de: 85000000000 => ??
-const example2 = null
-
+const example2 = 85e9
 // Notación exponencial de: 0.0025 => ??
-const example3 = null
-
+const example3 = 25e-4
 // Notación exponencial de: 0.000001 => ??
-const example4 = null
-
+const example4 = 1e-6
 // Algo está mal con el siguiente código, ¿puedes arreglarlo?
 const entero = '256.5px' // No puedes modificar esta línea
 // Devolver un entero 256
-const example5 = null
-
+const example5 = parseInt(entero)
 // Math.ceil(6.15) === ??
-const example6 = Math.ceil(6.15) === null
-
+const example6 = Math.ceil(6.15) === 7
 // Math.round(85.35) === ??
-const example7 = Math.round(85.35) === null
-
+const example7 = Math.round(85.35) === 85
 // Math.floor(4.99) === ??
-const example8 = Math.floor(4.99) === null
-
+const example8 = Math.floor(4.99) === 4
 // Math.round(10.5) === ??
-const example9 = Math.round(10.5) === null
-
+const example9 = Math.round(10.5) === 11
 // Math.ceil(12.72) === ??
-const example10 = Math.ceil(12.72) === null
-
+const example10 = Math.ceil(12.72) === 13
 // Math.floor(0.9) === ??
-const example11 = Math.floor(0.9) === null
-
+const example11 = Math.floor(0.9) === 0
 /*
   Ejercicio #2
   Realiza las siguientes funciones de números
@@ -49,24 +37,24 @@ const example11 = Math.floor(0.9) === null
   No debes crear otras variables (a menos que los requieras). Utiliza los parámetros que reciben las funciones.
   Recuerda utilizar return para devolver valores.
 */
-
 function validarNumero (numero) {
   // La función recibe un número y debe devolver true si es NaN, de lo contrario, false.
   // Ejemplo: in: (numero: 1) ~ out: => false
   // Ejemplo: in: (numero: NaN) ~ out: => true
   // Tu código:
+  return isNaN(numero)
 }
-
 function numeroRandom (inicio, final) {
   // La función recibe dos números y debe devolver un número aleatorio entre ambos números.
   // Ejemplo: in: (inicio: 1, final: 10) ~ out: => 5
   // Tu código:
+  return (Math.random() * 10 )
 }
-
 function redondear (numero, decimales) {
   // La función recibe un número y debe devolver un número redondeado a la cantidad de decimales indicada.
   // Ejemplo: in: (numero: 1.23456, decimales: 2) ~ out: => 1.23
   // Tu código:
+  return parseFloat(numero.toFixed(decimales))
 }
 
 // NO TOCAR ESTE CÓDIGO
